d85edc96b4f3c2cdaed399e692709320cefd858e
manhnguyen_:) <nguyenmanh19924@gmail.com>
2023-10-19 22:54:38 +0700

    #5

--- src/views/Exemple/Mycomponent.js
+++ src/views/Exemple/Mycomponent.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
 
 class Mycomponent extends React.Component {
-    render (){
-        return (
-            <div>Nguyen Tien Manh</div>
-        )
-    }
+  /**
+   * JSX  : Cho phép hàm js trả về html
+   * Chỉ trả về 1 phần tử
+   * Fragment : thay thế cho div tổng
+   * state : : Là 1 Object
+   */
+
+  state = {
+    name: "Manh",
+    channel: "Manh hoc react",
+  };
+  handOnChangeNamme = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  render() {
+    let name = "Mạnh";
+    return (
+      <React.Fragment>
+        <div className="first">
+          {console.log("my name : ", this.state.name)}
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handOnChangeNamme(event)}
+          ></input>
+          Nguyen Tien {this.state.name}
+        </div>
+        <div className="second">Manh dang xem o dau : {this.state.channel}</div>
+      </React.Fragment>
+    );
+  }
 }
 export default Mycomponent;
