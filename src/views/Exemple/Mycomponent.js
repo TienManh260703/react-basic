da1a00bea5e4e95c244a10fd70ece95f7a0693df
manhnguyen_:) <nguyenmanh19924@gmail.com>
2023-10-19 22:16:43 +0700

    Rect js

---
+++ src/views/Exemple/Mycomponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Mycomponent extends React.Component {
+    render (){
+        return (
+            <div>Nguyen Tien Manh</div>
+        )
+    }
+}
+export default Mycomponent;
